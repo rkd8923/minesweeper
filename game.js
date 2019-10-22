55b16988d3a2795c8e179afbc3d89cd8fd4c153b
Jiwon Kang <rkd8923@gmail.com>
2019-10-22 19:25:44 +0900

    ggg

--- game.js
+++ game.js
@@ -1,13 +1,14 @@
-console.log('testtesttest', document);
-function alertButton() {
-  alert('test');
+function initGame() {
+  let xy = document.gameSize;
+  console.log(xy.x.value, xy.y.value);
+  let map = document.createElement('div');
+  map.className = "map";
+  map.style.width = '110px';
+  for (let i=0; i<25; i++) {
+    let cell = document.createElement('div');
+    cell.className = "cell";
+    map.appendChild(cell);
+  }
+  document.body.appendChild(map);
+  console.log('initGame', document);
 }
-function click() {
-  console.log('click')
-  let parent = document.createElement('div');
-  let temp = document.createTextNode('testtesttest');
-  parent.appendChild(temp);
-  document.body.appendChild(parent);
-}
-
-document.getElementById('button1').addEventListener('click', click());
